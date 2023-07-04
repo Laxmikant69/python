e1ae63a974f682cf927879948d1fc07472aab105
Laxmikant69 <108482621+Laxmikant69@users.noreply.github.com>
2023-07-04 08:26:59 +0530

    Add files via upload

---
+++ stack_append_pop.py
@@ -0,0 +1,12 @@
+stack=[]
+stack.append('AMIT')
+stack.append('BHAKTI')
+stack.append('CHETAN')
+print('Initial stack')
+print(stack)
+
+print(stack.pop())
+print(stack.pop())
+
+print('\n stack after element are popped')
+print(stack)
